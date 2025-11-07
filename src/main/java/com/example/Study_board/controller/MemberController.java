9c8cd47a2fded71b87b7d405b69f0900060759eb
yang <ycj3294@gmail.com>
2025-11-07 17:29:43 +0900

    기능구현 및 로그인 기능 테스트 완료

--- src/main/java/com/example/Study_board/controller/MemberController.java
+++ src/main/java/com/example/Study_board/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.example.Study_board.controller;
 
 import com.example.Study_board.dto.LoginReq;
 import com.example.Study_board.dto.MemberRes;
+import com.example.Study_board.dto.MemberSignupReq;
 import com.example.Study_board.service.MemberService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.lang.reflect.Member;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,6 +29,7 @@ public class MemberController {
     public String loginPage() {
         return "login/login"; // resource/template/login.login.html 만들어줘야함
     }
+    // 로그인 처리
     @PostMapping("/login")
     public String login(@ModelAttribute LoginReq req, HttpSession session, Model model) {
         log.info("로그인 정보 : {}", req);
@@ -37,4 +41,31 @@ public class MemberController {
         session.setAttribute("loginMember", member);
         return "redirect:/posts";
     }
+
+    // 회원가입 페이지 이동
+    @GetMapping("/new")
+    public String signupPage() {
+        return "login/new";
+    }
+
+    // 회원 가입 처리
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute MemberSignupReq req, Model model) {
+        Long member = memberService.signup(req);
+        if(member == null) {
+            model.addAttribute("error", "이미 있는 이메일 입니다.");
+            return "login/new";
+        }
+        return "login/login";
+    }
+
+    // 전체 회원 목록 처리
+    @GetMapping("/list")
+    public String memberlist(HttpSession session, Model model) {
+        var loginMember = session.getAttribute("loginMember"); // 세션이 존재하는지 확인
+        List<MemberRes> list = memberService.list();
+        model.addAttribute("member",list);
+        return "login/list";
+    }
+
 }
