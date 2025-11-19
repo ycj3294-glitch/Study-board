55fd63d59346ea0c5847127a2b86c30892ae55e8
yang <ycj3294@gmail.com>
2025-11-19 12:49:49 +0900

    페이지네이션 구현(모아보기 공감글 검색)

--- src/main/java/com/example/Study_board/controller/SearchController.java
+++ src/main/java/com/example/Study_board/controller/SearchController.java
@@ -8,6 +8,9 @@ import com.example.Study_board.dto.SearchListRes;
 import com.example.Study_board.service.BoardService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -26,11 +29,22 @@ public class SearchController {
 
     // 검색 기능
     @GetMapping
-    public String search(@RequestParam String keyword, Model model) {
+    public String search(@RequestParam String keyword, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,Model model) {
         List<SearchListRes> result = boardService.search(keyword);
         model.addAttribute("p",keyword);
         model.addAttribute("result",result);
 
+        // 보여줄 페이지 데이터
+        int start = Math.min(page * size, result.size());
+        int end = Math.min(start + size, result.size());
+        List<SearchListRes> subList = result.subList(start, end);
+
+        Page<SearchListRes> searchPage = new PageImpl<>(subList, PageRequest.of(page, size), result.size());
+
+        // 화면에 표시될 게시글 리스트(10개씩 나눈거)
+        model.addAttribute("posts", subList);
+        // 페이지네이션용 page 객체
+        model.addAttribute("page", searchPage);
         return "/header/search";
     }
 }
